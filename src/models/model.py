9a305b959ae700612b3b56f64cd2b5eb85946407
Hoss Shakiba <hossshakiba@outlook.com>
2023-08-29 00:35:52 +0330

    Add cbam attention mechanism to model

---
+++ src/models/model.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+
+import torchvision.transforms.functional as TF
+
+from models.cbam import CBAM
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            CBAM(out_channels)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNET(nn.Module):
+    def __init__(self, in_channels=3, out_channels=23, features=[64, 128, 256, 512]):
+        super(UNET, self).__init__()
+        self.encoder = nn.ModuleList()
+        self.decoder = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # encoder
+        for feature in features:
+            self.encoder.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        # decoder
+        for feature in reversed(features):
+            self.decoder.append(
+                nn.ConvTranspose2d(
+                    feature*2, feature, kernel_size=2, stride=2,
+                )
+            )
+            self.decoder.append(DoubleConv(feature*2, feature))
+
+        # bottleneck
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+
+        # final layer
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        
+        for encoder_step in self.encoder:
+            x = encoder_step(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+
+        skip_connections.reverse()
+
+        for idx in range(0, len(self.decoder), 2):
+            x = self.decoder[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.decoder[idx+1](concat_skip)
+
+        return self.final_conv(x)
+
